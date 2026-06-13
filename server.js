04bf9df38c28f9854a4a4df30cbbe088a2648829
ky9xtd7nvy-ship-it <ky9xtd7nvy@privaterelay.appleid.com>
2026-06-13 11:27:23 +0200

    Crear funcion de arranque del servidor tras conexion con la base de datos

--- server.js
+++ server.js
@@ -1,7 +1,22 @@
 require ('dotenv').config();
 const app = require('./app');
+const pool = require('./src/config/db')
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en http://localhost:${PORT}`);
-});
+async function startServer() {
+    try {
+        await pool.query('SELECT 1')
+        console.log('Conexión establecida a la base de datos ');
+
+        app.listen(PORT, () => {
+            console.log(`Servidor escuchando en http://localhost:${PORT}`);
+        });
+    }
+    catch(error) {
+        console.error('Error al establecer conexión con la base de datos', error.message);   
+        process.exit(1);
+    }
+}
+
+startServer();
+
